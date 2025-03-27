b024b3c8b99c611eec3f66f81c66275beefda5d2
Hong Wei Tai <e1138406@u.nus.edu>
2025-03-27 15:10:53 +0800

    Add prompt-based analysis

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
+from pandasai import SmartDataframe 
+from pandasai.llm.openai import OpenAI
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
+llm = OpenAI(api_token=openai_api_key.strip()) 
 
 st.title("AI CSV/XLS Analyser")
 
@@ -14,10 +17,12 @@ n = st.text_input("How many rows would you like to view?", "")
 file = st.selectbox("Select which file you would like to view the rows of", options, index=0)
 
 if file and n.isdigit():
+    # Find selected file by user
     for f in uploaded_files:
         if f.name == file:
             selected_file = f
     
+    # Read and display n rows by user
     if selected_file:
         if selected_file.name.endswith(".csv"):
             df = pd.read_csv(selected_file)
@@ -25,9 +30,30 @@ if file and n.isdigit():
             df = pd.read_excel(selected_file)
         st.write(df.head(int(n)))
 
-    prompt = st.text_area("What do you want to know about the selected dataset?")
-    if prompt.strip() == "":
+    prompt = st.text_area("What do you want to know about the selected file?")
+
+    # Initialise session state
+    if "missing_prompt" not in st.session_state:
+        st.session_state.missing_prompt = False
+    if "generate_prompt" not in st.session_state:
+        st.session_state.generate_prompt = False
+
+    # On click event for sending prompt
+    def handle_send():
+        if prompt.strip() == "":
+            st.session_state.generate_prompt = False
+            st.session_state.missing_prompt = True
+        else:
+            st.session_state.generate_prompt = True
+            st.session_state.missing_prompt = False
+    
+    if st.session_state.missing_prompt:
         st.warning("Please enter a prompt!")
-    else:
-        st.write(prompt)
-    send = st.button("Send")
+
+    st.button("Send", on_click=handle_send)
+    
+    if st.session_state.generate_prompt:
+        with st.spinner("Generating..."):
+            sdf = SmartDataframe(df, config={"llm": llm})
+            response = sdf.chat(prompt, output_type="string")
+            st.write(response)
